d22b6ff0890aad134551a02e6aac3c1d0ab76d27
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-06-18 23:32:23 -0300

    Feito

--- get-temperature.py
+++ get-temperature.py
@@ -1,28 +1,27 @@
-def getTemperature (cep):
-    import requests
 
-    API_key = '15e582cd6693c8f08119cd35d98c9a8f' #Código da API de temperatura
+import requests
 
-    cep = input('Digite o CEP: ')
-    if len(cep) != 8:
-        print('CEP inválido')
-        exit()
+API_key = '15e582cd6693c8f08119cd35d98c9a8f' #Código da API de temperatura
 
-    r = requests.get(f'https://viacep.com.br/ws/{cep}/json/') #API do CEP
+cep = input('Digite o CEP: ')
+if len(cep) != 8:
+    print('CEP inválido')
+    exit()
 
-    dicionario = r.json()
+r = requests.get(f'https://viacep.com.br/ws/{cep}/json/') #API do CEP
 
-    city_name = dicionario['localidade'] #Variável que pega o nome da cidade pelo json do CEP
+dicionario = r.json()
 
-    rua = dicionario['logradouro'] #VARIÁVEL que pega a Rua pelo json do CEP
+city_name = dicionario['localidade'] #Variável que pega o nome da cidade pelo json do CEP
 
-    link = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&lang=pt_br' #Link da API tempo
+rua = dicionario['logradouro'] #VARIÁVEL que pega a Rua pelo json do CEP
 
-    request = requests.get(link)
-    request_dic = request.json()
-    description = request_dic['weather'][0]['description'] #Pegando a lista weather
-    temperature = request_dic['main']['temp'] - 273.15 #Pegando a temperatura pela chave 'temp'
-    print(f'A temperatura atual da cidade {city_name.upper()}, na Rua {rua}, é {temperature:.0f}ºC')
+link = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&lang=pt_br' #Link da API tempo
+
+request = requests.get(link)
+request_dic = request.json()
+description = request_dic['weather'][0]['description'] #Pegando a lista weather
+temperature = request_dic['main']['temp'] - 273.15 #Pegando a temperatura pela chave 'temp'
+print(f'A temperatura atual da cidade {city_name.upper()}, na Rua {rua}, é {temperature:.0f}ºC')
 
 
-getTemperature('')
